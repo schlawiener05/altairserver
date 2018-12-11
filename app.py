1949a8c91eea80f9ccfa3ee91f10f0badade58aa
Janosch Achenbach <janosch@janoschs-mbp.lan>
2018-12-11 22:20:59 +0100

    played with tooltip

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask import jsonify
 from flask import render_template
 import altair as alt
+from vega_datasets import data
 
 app = Flask(__name__)
 
 cars = alt.load_dataset('cars')
+source = data.barley()
 
 @app.route('/')
 def hello_world():
@@ -19,14 +21,20 @@ def index():
 
 @app.route('/test')
 def test():
-    cjson = create_chart
+    cjson = create_chart()
     return jsonify(cjson)
 
+@app.route('/testdata')
+def testdata():
+    return jsonify(source.to_dict('records'))
+
 def create_chart():
-    c = alt.Chart(cars).mark_point().encode(
-        x='Horsepower',
-        y='Miles_per_Gallon',
-        color='Origin',
+    c=alt.Chart(source).mark_bar().encode(
+        x='yield:Q',
+        y='year:O',
+        color='year:N',
+        row='site:N',
+        tooltip=['yield', 'year']
     )
     cjson = c.to_dict()
     return cjson
